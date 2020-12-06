1f69684e766d297dfdf35838f29871d286eff921
The Jabberwock <zazyzaya@gmail.com>
2020-12-06 14:23:58 -0500

    rearranging

--- generate.py
+++ generate.py
@@ -1,10 +1,13 @@
+import os 
 import torch
 import matplotlib.pyplot as plt
 
 from torchvision.transforms import ToPILImage
 
+modelf = os.path.join('models', 'gen_c0_128in128out.model')
+
 to_img = ToPILImage()
-G = torch.load('generator.model')
+G = torch.load(modelf)
 
 for i in range(5):
     plt.imshow(to_img(G.generate_random(2)[0]))
